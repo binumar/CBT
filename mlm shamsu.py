a6d66ad15fcfe0f41ab953617540daf352711ada
binumar <61487840+binumar@users.noreply.github.com>
2025-03-08 14:27:11 -0800

    Add files via upload

---
+++ mlm shamsu.py
@@ -0,0 +1,23 @@
+import numpy as np
+import pandas as pd
+from scipy.stats import entropy
+from sklearn.metrics import mean_squared_error
+from typing import List, Dict, Tuple
+
+class TimeSeriesEvaluator:
+    def __init__(self, predictions: np.ndarray, ground_truth: np.ndarray):
+        """
+        Initialize evaluator with predictions and ground truth
+        predictions: shape (n_samples, n_features)
+        ground_truth: shape (n_samples, n_features)
+        """
+        self.predictions = predictions
+        self.
+
+
+https://paperswithcode.com/sota/mathematical-reasoning-on-aime24
+https://paperswithcode.com/paper/s1-simple-test-time-scaling#
+
+https://colab.research.google.com/github/liam-sbhoo/tabpfn-time-series/blob/main/demo.ipynb#scrollTo=THNg6-7KHS_3
+https://colab.research.google.com/drive/1GAfwbJs2Y1dgGGsxrQyQg2G7CRH5NgN3?usp=sharing
+https://colab.research.google.com/github/liam-sbhoo/tabpfn-time-series/blob/main/demo.ipynb
